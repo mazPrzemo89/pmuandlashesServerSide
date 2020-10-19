49d58350161c1c1b5a20a427039ab8bad0852e27
mazPrzemo89 <maz89prz@gmail.com>
2020-10-19 13:40:56 +0100

    controllers/bookingTimes.js - readDefault function will return an object with error property

--- controllers/bookingTimes.js
+++ controllers/bookingTimes.js
@@ -1,5 +1,5 @@
 const BookingTimes = require('../models/bookingTimes')
-const {errorHandler} = require('../helpers/dbErrorHandler')
+const { errorHandler } = require('../helpers/dbErrorHandler')
 const times = require('../helpers/bookingTimes')
 
 const bookingTimes = {
@@ -10,94 +10,99 @@ const bookingTimes = {
 
 exports.createDefault = (req, res) => {
     let data = new BookingTimes(bookingTimes)
-    data.save((err, data)=>{
-        if(err) {
+    data.save((err, data) => {
+        if (err) {
             return res.status(400).json({
                 error: errorHandler(err)
             })
         }
-       return res.json(data)
+        return res.json(data)
     })
 }
 
 exports.readDefault = (req, res) => {
-    BookingTimes.find({"name":"workingTimesDefault"}).exec((err, data) => {
-        if(err){
+    BookingTimes.find({ "name": "workingTimesDefault" }).exec((err, data) => {
+        console.log(data)
+        if (err) {
             return res.status(400).json({
                 error: errorHandler(err)
             })
         } else {
-            return res.json(data[0].bookings)
+            if (data.length > 0) {
+                return res.json(data[0].bookings)
+            } else {
+                res.status(200).json({ "error": true })
+            }
+
         }
     })
 }
 
 exports.createCustom = (req, res) => {
     console.log('CREATE CUSTOM')
-    let update = {name:"workingTimes", bookings: req.body.bookings, day: req.body.day }
-    const separtator =   { time: '--', type: '', isNotBooked: false, break: false ,origin: true }
-  
+    let update = { name: "workingTimes", bookings: req.body.bookings, day: req.body.day }
+    const separtator = { time: '--', type: '', isNotBooked: false, break: false, origin: true }
+
     const array = new Array()
-    
-    update.bookings.forEach((element,index,arr)=> {
+
+    update.bookings.forEach((element, index, arr) => {
 
         array.push(element)
-        if( arr[index].hasOwnProperty('position') && arr[index+1].hasOwnProperty('position') && 
-            (arr[index+1].position-arr[index].position > 1)
+        if (arr[index].hasOwnProperty('position') && arr[index + 1].hasOwnProperty('position') &&
+            (arr[index + 1].position - arr[index].position > 1)
+        ) {
+            array.push(arr[index])
+            if (array[array.indexOf(arr[index])] === array[array.indexOf(arr[index]) + 1]) (
+                array.splice(array.indexOf(arr[index]), 1)
             )
-            {       
-                    array.push(arr[index])
-                    if(array[array.indexOf(arr[index])] === array[array.indexOf(arr[index])+1])(
-                        array.splice(array.indexOf(arr[index]),1)
-                    )
-                    for(let k=0;k<arr[index+1].position-arr[index].position-1;k++){
-                        console.log(index)
-                        array.splice(index+1,0,separtator)  
-                    }    
-            }   
+            for (let k = 0; k < arr[index + 1].position - arr[index].position - 1; k++) {
+                console.log(index)
+                array.splice(index + 1, 0, separtator)
+            }
+        }
     });
-   
+
 
     update.bookings = array
-    
+
     BookingTimes.findOneAndUpdate(
-        {"day":req.body.day}, 
+        { "day": req.body.day },
         update,
         {
-        upsert: true,
-        useFindAndModify: false
+            upsert: true,
+            useFindAndModify: false
         }
-      ).exec((err, data) => {
-        if(err) {
+    ).exec((err, data) => {
+        if (err) {
             return res.status(400).json({
                 error: errorHandler(err)
             })
         }
-        if(data === null){
+        if (data === null) {
             let bookings = new BookingTimes({
                 name: "workingTimes",
                 bookings: req.body.bookings,
                 day: req.body.day
-            }) 
-            bookings.save((err, data)=>{
-                if(err) {
+            })
+            bookings.save((err, data) => {
+                if (err) {
                     return res.status(400).json({
                         error: errorHandler(err)
                     })
                 }
-                 res.json(data)
+                res.json(data)
             })
         } else {
             return res.json(data)
         }
-        
+
     })
 
 }
 
 exports.readCustom = (req, res) => {
-    BookingTimes.find({"name":"workingTimes"}).exec((err, data) => {
-        if(err){
+    BookingTimes.find({ "name": "workingTimes" }).exec((err, data) => {
+        if (err) {
             return res.status(400).json({
                 error: errorHandler(err)
             })
@@ -111,18 +116,18 @@ exports.readCustom = (req, res) => {
 exports.list = (req, res) => {
     let response = []
     BookingTimes.find().exec((err, bookingTimes) => {
-        if(err || !bookingTimes) {
+        if (err || !bookingTimes) {
             return res.status(400).json({
                 error: 'Times not found'
             })
         } else {
-           response = bookingTimes.map((el)=>{
-                    if(el.day !== "all" && el.day !== "default" ){
-                        return { 
-                            day:el.day,
-                            id: el._id
-                            }
-                    } 
+            response = bookingTimes.map((el) => {
+                if (el.day !== "all" && el.day !== "default") {
+                    return {
+                        day: el.day,
+                        id: el._id
+                    }
+                }
             })
             return res.json(response)
         }
@@ -131,13 +136,13 @@ exports.list = (req, res) => {
 
 exports.remove = (req, res) => {
     BookingTimes.findById(req.body.id).exec((err, bookingTimes) => {
-        if(err || !bookingTimes) {
+        if (err || !bookingTimes) {
             return res.status(400).json({
                 error: 'Category not found'
             })
         }
         bookingTimes.remove((err, deletedTimes) => {
-            if(err) {
+            if (err) {
                 return res.status(400).json({
                     error: errorHandler(err)
                 })
